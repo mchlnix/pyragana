408c894a95c1518d193adbae6364da2522e389ed
Michael Nix <mchl.nix@googlemail.com>
2020-04-16 13:57:41 +0200

    adds setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyragana',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/mchlnix/pyragana',
+    license='',
+    author='Michael Nix',
+    author_email='mchl.nix@googlemail.com',
+    description='Type hiragana and katakana characters everywhere.'
+)
